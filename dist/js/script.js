174494dbc435bc407f9707535bfb3bbcfb652202
Fran Ruiz-Tagle <francisca.ruiztagle@gmail.com>
2017-08-10 11:18:51 -0400

    probando api

--- dist/js/script.js
+++ dist/js/script.js
@@ -20255,4 +20255,32 @@ if (jQuery) {
 
 $(document).ready(function() {
 
+    //solo para llamar pelicula listadas por director
+
+    $.ajax({
+            url: `https://netflixroulette.net/api/api.php?director=Steven%20Spielberg`,
+            type: 'GET',
+            datatype: 'JSON',
+        })
+        .done(function(response) {
+            console.log(response); //todo lo que haga ocurre acá
+
+            response.forEach(function(data) {
+                console.log(data.show_title);
+                console.log(data.release_year);
+                console.log(data.category);
+            })
+
+        })
+        .fail(function() {
+            alert('Fallo');
+        })
+        .always(function() {
+            console.log('Yeii')
+        });
+
+
+});
+$(document).ready(function() {
+
 });
