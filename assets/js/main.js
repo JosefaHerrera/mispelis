7ce6ac5936cd24c72c8616f8f80a2de7b912e778
Natalia Garrido <natgramirez@outlook.com>
2017-08-10 11:12:36 -0400

    obteniendo datos para lista-peliculas.html desde la api

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-    console.log('asdsh')
     $.ajax({
             url: `https://netflixroulette.net/api/api.php?director=Steven%20Spielberg`,
             type: 'GET',
@@ -7,6 +6,22 @@ $(document).ready(function() {
         })
         .done(function(response) {
             console.log(response);
+            response.forEach(function(dataDirector) {
+                // Lista de peliculas
+                console.log('nombre pelicula ->', dataDirector.show_title);
+
+                // año de lanzamiento
+                console.log('año ->', dataDirector.release_year);
+
+                // categoria
+                console.log('categoria ->', dataDirector.category);
+
+                // duracion
+                console.log('duración ->', dataDirector.runtime);
+
+                // director
+                console.log('director ->', dataDirector.director);
+            });
         })
         .fail(function() {
             alert('Fallo');
--- dist/js/script.js
+++ dist/js/script.js
@@ -20254,7 +20254,6 @@ if (jQuery) {
 })(jQuery);
 
 $(document).ready(function() {
-    console.log('asdsh')
     $.ajax({
             url: `https://netflixroulette.net/api/api.php?director=Steven%20Spielberg`,
             type: 'GET',
@@ -20262,6 +20261,22 @@ $(document).ready(function() {
         })
         .done(function(response) {
             console.log(response);
+            response.forEach(function(dataDirector) {
+                // Lista de peliculas
+                console.log('nombre pelicula ->', dataDirector.show_title);
+
+                // año de lanzamiento
+                console.log('año ->', dataDirector.release_year);
+
+                // categoria
+                console.log('categoria ->', dataDirector.category);
+
+                // duracion
+                console.log('duración ->', dataDirector.runtime);
+
+                // director
+                console.log('director ->', dataDirector.director);
+            });
         })
         .fail(function() {
             alert('Fallo');
