f41ee320ca0a6ecbc12cb5cecc76cba18a94072c
Natalia Garrido <natgramirez@outlook.com>
2017-08-10 23:50:40 -0400

    agregando filtros para las peliculas

--- dist/js/script.js
+++ dist/js/script.js
@@ -20336,54 +20336,60 @@ $(document).ready(function() {
             response.forEach(function(dataDirector, index) {
 
                 $('#select-category').append(`
-                    <option value="">${dataDirector.category}</option>
+                    <option value="${dataDirector.category}">${dataDirector.category}</option>
                 `);
-                // Lista de peliculas
-                // console.log('nombre pelicula ->', dataDirector.show_title);
-
-                // año de lanzamiento
-                // console.log('año ->', dataDirector.release_year);
-
-                // categoria
-                // console.log('categoria ->', dataDirector.category);
-
-                // duracion
-                // console.log('duración ->', dataDirector.runtime);
-
-                // director
-                // console.log('director ->', dataDirector.director);
-
-                //dibujar info en la página
 
                 $(".gris").append(
-                    `<div class="col s12">
+                    `<div class="${dataDirector.show_id} contenedor-director">
                         <div class="col s12">
-                            <div class="col s8">
-                                <span class="movie big">${dataDirector.show_title}</span>
-                                <span class="year small">${dataDirector.release_year}</span> |
-                                <span class="category small">${dataDirector.category}</span>
+                            <div class="col s12">
+                                <div class="col s8">
+                                    <span class="movie big">${dataDirector.show_title}</span>
+                                    <span class="year small">${dataDirector.release_year}</span> |
+                                    <span class="category small">${dataDirector.category}</span>
+                                </div>
+                                <div class="col s4 offset-s10">
+                                    <a id="favorite" class="boton waves-light"><i class="material-icons">stars</i></a>                                
+                                </div>
                             </div>
-                            <div class="col s4 offset-s10">
-                                <a id="favorite" class="boton waves-light"><i class="material-icons">stars</i></a>                                
+                        </div>
+                        <div class="row dat">
+                            <div class="col s12">
+                                <span class="time small">${dataDirector.runtime} </span>
+                                <span class="director small">${dataDirector.director} </span>
+                                <span class="rating small">${dataDirector.rating} </span>                            
+                            </div>
+                            <div class="right">
+                                <button type="" class="btn-details" data-name="${dataDirector.show_title}">details</button>    
                             </div>
                         </div>
                     </div>
-                    <div class="row dat">
-                        <div class="col s12">
-                            <span class="time small">${dataDirector.runtime} </span>
-                            <span class="director small">${dataDirector.director} </span>
-                            <span class="rating small">${dataDirector.rating} </span>                            
-                        </div>
-                        <div class="right">
-                            <button type="" class="btn-details" data-name="${dataDirector.show_title}">details</button>    
-                        </div>
-                    </div>
-                    <hr>`
+                  `
                 );
+
+                //capturar el valor seleccionado por el usuario
+                $('#select-category').on('change', function() {
+                    //valor seleccionado por el usuario
+                    //console.log($(this).val());
+                    var valorActual = $(this).val();
+                    // desaparecer todos los que no son de esta categoria
+                    if (valorActual === `${dataDirector.category}`) {
+                        console.log(`${dataDirector.category}`);
+                        $(`.${dataDirector.show_id}`).show();
+
+                    } else {
+                        $(`.${dataDirector.show_id}`).hide();
+                        console.log('asd', `${dataDirector.category}`);
+
+                    }
+                });
 
             });
             //inicializacion select
             $('select').material_select();
+
+
+
 
             // hacer un evento por el boton de la vista lista-peliculas.html
             $('.btn-details').on('click', function() {
