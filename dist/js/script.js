07bdd5f55796260316aba9f25f58b1a3acc15094
JosefaHerrera <josefa@linuxero.cl>
2017-08-11 09:54:56 -0400

    commit

--- dist/js/script.js
+++ dist/js/script.js
@@ -20253,38 +20253,6 @@ if (jQuery) {
   };
 })(jQuery);
 
-
-    $('#btn-enviar').click(function(){
-        /*Captura de datos escrito en los inputs*/
-        var nom = document.getElementById("first_name").value;
-        var ape = document.getElementById("last_name").value;
-        var ema = document.getElementById("email").value;
-        var pai = document.getElementById("country").value;
-        /*Guardando las variables creadas anteriorirmente en las nuevas variables del LocalStorage*/
-        //Variable localstorage, VALOR ASGINADO A LA VARIBLE
-        localStorage.setItem("Nombre", nom);
-        localStorage.setItem("Apellido", ape);
-        localStorage.setItem("Email", ema);
-      	localStorage.setItem("Pais", pai);
-    });
-
-
-$(document).ready(function(){
-    $('#prueba').click(function(){
-        /*Guarda en variables locales lo que està alamacenado en las varibles del localstorage*/
-        var nombre = localStorage.getItem("Nombre");
-        var apellido = localStorage.getItem("Apellido");
-        var correo = localStorage.getItem("Email");
-        var pais = localStorage.getItem("Pais");
-        /*Mostrar con un innerhtml para jquery la variable local*/
-        $('#first_name').html(nombre);
-        $('#last_name').html(apellido);
- 		$('#mail').html(correo);
- 		$('#country').html(pais);
-    });
-});
-
-
 // $(document).ready(function() {
 
 //     //solo para llamar pelicula listadas por director
@@ -20313,7 +20281,6 @@ $(document).ready(function(){
 
 
 // });
-
 $(document).ready(function() {
     var params = new URLSearchParams(location.search.slice(1));
 
@@ -20356,6 +20323,36 @@ $(document).ready(function() {
     }
 
 });
+    $('#btn-enviar').click(function(){
+        /*Captura de datos escrito en los inputs*/
+        var nom = document.getElementById("first_name").value;
+        var ape = document.getElementById("last_name").value;
+        var ema = document.getElementById("email").value;
+        var pai = document.getElementById("country").value;
+        /*Guardando las variables creadas anteriorirmente en las nuevas variables del LocalStorage*/
+        //Variable localstorage, VALOR ASGINADO A LA VARIBLE
+        localStorage.setItem("Nombre", nom);
+        localStorage.setItem("Apellido", ape);
+        localStorage.setItem("Email", ema);
+      	localStorage.setItem("Pais", pai);
+    });
+
+
+$(document).ready(function(){
+    $('#prueba').click(function(){
+        /*Guarda en variables locales lo que està alamacenado en las varibles del localstorage*/
+        var nombre = localStorage.getItem("Nombre");
+        var apellido = localStorage.getItem("Apellido");
+        var correo = localStorage.getItem("Email");
+        var pais = localStorage.getItem("Pais");
+        /*Mostrar con un innerhtml para jquery la variable local*/
+        $('#first_name').html(nombre);
+        $('#last_name').html(apellido);
+ 		$('#mail').html(correo);
+ 		$('#country').html(pais);
+    });
+});
+
 $(document).ready(function() {
 
 
